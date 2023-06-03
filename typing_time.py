47013443c13ed0541478aec5f7cd13d4fddf024e
MD MONJUR MORSHED <mmt3719@gmail.com>
2023-06-03 18:28:25 +0600

    single row keyboard algorithm

---
+++ typing_time.py
@@ -0,0 +1,15 @@
+def calculate_timing(digits,target):
+    
+  digit={}
+  for x,y in enumerate(digits):
+    digit[y]=x
+  total_time=0
+  prev=0
+  for i in target:
+    total_time+= abs(digit[i]-prev)
+    prev=digit[i]
+  return total_time  
+a="8675309124"
+b="12"
+output=calculate_timing(a,b)
+print(output)
