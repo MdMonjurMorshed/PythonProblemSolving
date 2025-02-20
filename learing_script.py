405b7d17a329c5a1a7e70d51620addfcbe04f6b5
Md Monjur Morshed <mmt3719@gmail.com>
2025-02-20 20:54:00 +0600

    learned about map() function

--- learing_script.py
+++ learing_script.py
@@ -10,3 +10,13 @@ SecondClass.x = 2
 print(FirstClass.x,SecondClass.x,ThirdClass.x)
 FirstClass.x = 3
 print(FirstClass.x,SecondClass.x,ThirdClass.x)
+
+# map() function
+
+'''map takes function and single or multiple iterable.
+iterable could be list or tuple. 
+function in the map uses every elements from the iterable. 
+map returns an another iterable. '''
+
+output_map = map(lamda x:x**2,[1,2,3])
+print(output_map)
